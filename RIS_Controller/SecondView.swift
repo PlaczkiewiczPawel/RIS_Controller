3e370fc0da54eaec072a1017ed1c8d39f7bb67fe
PlaczkiewiczPawel <162739607+PlaczkiewiczPawel@users.noreply.github.com>
2024-12-20 20:59:39 +0100

    ContentView, SecondView

---
+++ RIS_Controller/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  RIS_Controller
+//
+//  Created by Paweł Płaczkiewicz on 20/12/2024.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
